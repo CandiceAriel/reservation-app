d509a4a4c5c668fa88c34267352a84fd1cbbb7dc
Candice Ariel <candicewangsa@gmail.com>
2026-06-18 14:52:49 +0700

    Add homepage

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='bg-navy'>
+        <h2 className='text-text-light'>Reserve Your <br/>Perfect Table</h2>
+        <p className='text-text-light'>Experience warm hospitality and exquisite cuisine at our intimate dining venue. Book your table in seconds and let us take care of the rest.</p>
+
+    </div>
+  )
+}
+
+export default Home
